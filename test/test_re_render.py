bc15262cee65c4955e12f81b1556ac16e631e16e
gulan <glen.wilder@gmail.com>
2018-09-29 09:45:48 -0700

    write some regex rendering tests

--- test/test_re_render.py
+++ test/test_re_render.py
@@ -1,5 +1,31 @@
 #!python
 
+import io
 from jsdtools.regex.render import (printer, walk, print_ast)
+from jsdtools.regex.parse import (ParsingError, RegexParser, parse_one)
 
-def test_xxx(): pass
+def my_print_ast(ast):
+    # render to string with no newline
+    sf = io.StringIO()
+    p = printer(out=sf)
+    walk(p, ast)
+    r = sf.getvalue()
+    sf.close()
+    return r
+    
+def test_inverse():
+    def identity(r):
+        return my_print_ast(parse_one(r)) == r
+    assert identity('a')
+    assert identity('(a . b)')
+    assert identity('(a . b . c)')
+    assert identity('((a . b) . c)')
+    assert identity('(a . (b . c))')
+    
+    assert identity('(a | b)')
+    assert identity('(a | b | c)')
+    assert identity('((a | b) | c)')
+    assert identity('(a | (b | c))')
+
+    assert identity('a*')
+    assert identity('a**')
